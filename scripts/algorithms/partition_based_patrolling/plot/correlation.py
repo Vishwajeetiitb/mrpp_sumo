bf68afc72308d8ef1ca7feb5c0540dbca3968cf1
vishwajeetiitb <vishwajeetiitb.official@gmail.com>
2023-06-30 20:45:18 +0530

    test

--- scripts/algorithms/partition_based_patrolling/plot/correlation.py
+++ scripts/algorithms/partition_based_patrolling/plot/correlation.py
@@ -19,8 +19,8 @@ graph_name = 'pipeline2'
 dirname = rospkg.RosPack().get_path('mrpp_sumo')
 no_agents = 6
 deploy_tag = 'graph'
-device_ranges = [100,240,1000]
-device_names = ['Zigbee','BLE','LoRa']
+device_ranges = [100, 240, 1000]
+device_names = ['Zigbee', 'BLE', 'LoRa']
 # available_comparisons = ['Idleness', 'Worst Idleness']
 # comparison_parameter_index = 0
 # scater_nodes_algo_index =  2# putting scatter for only one algo is better otherwise mess put -1 if don't require node scatter
@@ -38,71 +38,95 @@ color_list = [
     '#bcbd22',  # curry yellow-green
     '#17becf'   # blue-teal
 ]
+
+colorscale_list = ['aggrnyl', 'agsunset', 'algae', 'amp', 'armyrose', 'balance',
+             'blackbody', 'bluered', 'blues', 'blugrn', 'bluyl', 'brbg',
+             'brwnyl', 'bugn', 'bupu', 'burg', 'burgyl', 'cividis', 'curl',
+             'darkmint', 'deep', 'delta', 'dense', 'earth', 'edge', 'electric',
+             'emrld', 'fall', 'geyser', 'gnbu', 'gray', 'greens', 'greys',
+             'haline', 'hot', 'hsv', 'ice', 'icefire', 'inferno', 'jet',
+             'magenta', 'magma', 'matter', 'mint', 'mrybm', 'mygbm', 'oranges',
+             'orrd', 'oryel', 'oxy', 'peach', 'phase', 'picnic', 'pinkyl',
+             'piyg', 'plasma', 'plotly3', 'portland', 'prgn', 'pubu', 'pubugn',
+             'puor', 'purd', 'purp', 'purples', 'purpor', 'rainbow', 'rdbu',
+             'rdgy', 'rdpu', 'rdylbu', 'rdylgn', 'redor', 'reds', 'solar',
+             'spectral', 'speed', 'sunset', 'sunsetdark', 'teal', 'tealgrn',
+             'tealrose', 'tempo', 'temps', 'thermal', 'tropic', 'turbid',
+             'turbo', 'twilight', 'viridis', 'ylgn', 'ylgnbu', 'ylorbr',
+             'ylorrd']
 # names = algo_list
 # names = names.extend(['Range vs Deviation'])
 
-fig = make_subplots(rows=row_size, cols=col_size,subplot_titles=device_names)
-fig.update_layout(title='Correlation of Instantaneous Graph Idle between agents ({})'.format(graph_name),title_x=0.5)
+fig = make_subplots(rows=row_size, cols=col_size, subplot_titles=device_names)
+fig.update_layout(title='Correlation of Instantaneous Graph Idle between agents ({})'.format(
+    graph_name), title_x=0.5)
 
-for idx,device_range in enumerate(device_ranges):
-    path = '{}/post_process/{}/on_{}/{}m_range'.format(dirname,graph_name,deploy_tag,device_range)
+for idx, device_range in enumerate(device_ranges):
+    path = '{}/post_process/{}/on_{}/{}m_range'.format(
+        dirname, graph_name, deploy_tag, device_range)
     n = [int(filename.split('_')[0]) for filename in os.listdir(path)]
     df = pd.DataFrame()
-    results_path = '{}/{}_base_stations/{}_agents/run_0'.format(path,min(n),no_agents)
-    agent_masterdata = np.load('{}/agent_masterdata_final.npz'.format(results_path))['arr_0']
+    results_path = '{}/{}_base_stations/{}_agents/run_0'.format(
+        path, min(n), no_agents)
+    agent_masterdata = np.load(
+        '{}/agent_masterdata_final.npz'.format(results_path))['arr_0']
     print(agent_masterdata.shape)
     stamps = np.load('{}/stamps_final.npz'.format(results_path))['arr_0']
-    agent_masterdata_graph_idle = np.transpose(np.mean(agent_masterdata,axis=2))
+    agent_masterdata_graph_idle = np.transpose(
+        np.mean(agent_masterdata, axis=2))
     # print(agent_masterdata_graph_idle.shape)
-    corr= np.corrcoef(agent_masterdata_graph_idle)
-    fig.add_trace(go.Heatmap(z=corr,text=np.around(corr,2),texttemplate="%{text}",showlegend=(False if idx==0 else False),showscale=(True if idx==0 else False),zmax=1,zmin=0),row=int(idx/col_size)+1,col=idx%col_size+1)
+    corr = np.corrcoef(agent_masterdata_graph_idle)
+    fig.add_trace(go.Heatmap(z=corr,text=np.around(corr, 2), texttemplate="%{text}", showlegend=(False if idx == 0 else False), showscale=(
+        True if idx == 0 else False), zmax=1, zmin=0), row=int(idx/col_size)+1, col=idx % col_size+1)
 
 layout = Layout(
     paper_bgcolor='rgba(0,0,0,0)',
     plot_bgcolor='rgba(0,0,0,0)'
 )
 
-fig.update_layout(paper_bgcolor='rgba(0,0,0,0)',plot_bgcolor='rgba(0,0,0,0)')
+fig.update_layout(paper_bgcolor='rgba(0,0,0,0)', plot_bgcolor='rgba(0,0,0,0)')
 # fig.update_xaxes(scaleanchor = "y",scaleratio = 1,showgrid=False)
 # fig.update_yaxes(scaleanchor = "x",scaleratio = 1,showgrid=False)
-# fig.update_layout(yaxis1 = dict(range=[0, 6]),yaxis2 = dict(range=[0, 6]),yaxis3 = dict(range=[0, 6])) 
-fig.update_layout(height=700,width=2100)
+# fig.update_layout(yaxis1 = dict(range=[0, 6]),yaxis2 = dict(range=[0, 6]),yaxis3 = dict(range=[0, 6]))
+fig.update_layout(height=700, width=2100)
 # fig.update_layout(xaxis_title = dict(text=))
 fig.update_layout(
     xaxis1=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     ),
 
     xaxis2=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     ),
 
     xaxis3=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     ),
 
     yaxis1=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     ),
 
     yaxis2=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     ),
 
     yaxis3=dict(
         tickmode='array',
-        tickvals = [i for i in range(no_agents)],
+        tickvals=[i for i in range(no_agents)],
         ticktext=['Agent_{}'.format(i+1) for i in range(no_agents)]
     )
 )
+fig.update_xaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
+fig.update_yaxes(showline=True, linewidth=1, linecolor='black', mirror=True)
 iplot(fig)
--- scripts/algorithms/partition_based_patrolling/plot/instantaneous_graph_compare2.py
+++ scripts/algorithms/partition_based_patrolling/plot/instantaneous_graph_compare2.py
@@ -14,14 +14,15 @@ import chart_studio.plotly as py
 from plotly.offline import iplot
 
 
-graph_name = 'pipeline3'
-no_agents_list = [1,3,6,9,12,15]
+graph_name = 'pipeline2'
+no_agents_list = [1, 3, 6, 9, 12, 15]
 deploy_tag = 'graph'
-device_ranges = [100,240,1000]
-device_names = ['Zigbee','BLE','LoRa']
+device_ranges = [100, 240, 1000]
+device_names = ['Zigbee', 'BLE', 'LoRa']
 available_comparisons = ['Idleness', 'Worst Idleness']
 comparison_parameter_index = 0
-scater_nodes_algo_index =  -1# putting scatter for only one algo is better otherwise mess put -1 if don't require node scatter
+# putting scatter for only one algo is better otherwise mess put -1 if don't require node scatter
+scater_nodes_algo_index = -1
 row_size = 1
 col_size = 3
 
@@ -38,58 +39,64 @@ color_list = [
     '#17becf'   # blue-teal
 ]
 dirname = rospkg.RosPack().get_path('mrpp_sumo')
-fig = make_subplots(rows=row_size, cols=col_size,subplot_titles=device_names,vertical_spacing=0.1,horizontal_spacing=0.05)
+fig = make_subplots(rows=row_size, cols=col_size, subplot_titles=device_names,
+                    vertical_spacing=0.1, horizontal_spacing=0.05)
 
 
-for m,no_agents in enumerate(no_agents_list):
-    
-    for idx,device_range in enumerate(device_ranges):
-        path = '{}/post_process/{}/on_{}/{}m_range'.format(dirname,graph_name,deploy_tag,device_range)
+for m, no_agents in enumerate(no_agents_list):
+
+    for idx, device_range in enumerate(device_ranges):
+        path = '{}/post_process/{}/on_{}/{}m_range'.format(
+            dirname, graph_name, deploy_tag, device_range)
         n = [int(filename.split('_')[0]) for filename in os.listdir(path)]
         df = pd.DataFrame()
-        results_path = '{}/{}_base_stations/{}_agents/run_0'.format(path,min(n),no_agents)
+        results_path = '{}/{}_base_stations/{}_agents/run_0'.format(
+            path, min(n), no_agents)
         idle = np.load('{}/data_final.npz'.format(results_path))['arr_0']
         stamps = np.load('{}/stamps_final.npz'.format(results_path))['arr_0']
-        
-        if comparison_parameter_index == 0 : 
+
+        if comparison_parameter_index == 0:
             # val = np.average(idle,axis=1)
-            val = np.average(idle,axis=1).cumsum()
-            val = val/np.arange(1,val.shape[0]+1)
-        elif comparison_parameter_index == 1 : val = np.max(idle,axis=1)
-        fig.add_trace(go.Scatter(x=stamps, y=val,mode='lines',marker=dict(color=color_list[m]),legendgroup=m+1,name='{} Agents'.format(no_agents),showlegend=(True if idx==0 else False)),row=int(idx/col_size)+1,col=idx%col_size+1)
-        if scater_nodes_algo_index !=-1 and scater_nodes_algo_index ==m:
+            val = np.average(idle, axis=1).cumsum()
+            val = val/np.arange(1, val.shape[0]+1)
+        elif comparison_parameter_index == 1:
+            val = np.max(idle, axis=1)
+        fig.add_trace(go.Scatter(x=stamps, y=val, mode='lines', marker=dict(color=color_list[m]), legendgroup=m+1, name='{} Agents'.format(
+            no_agents), showlegend=(True if idx == 0 else False)), row=int(idx/col_size)+1, col=idx % col_size+1)
+        fig.update_xaxes(tickfont=dict(size=20), row=int(idx/col_size)+1, col=idx % col_size+1)
+        fig.update_yaxes(tickfont=dict(size=20), row=int(idx/col_size)+1, col=idx % col_size+1)
+        if scater_nodes_algo_index != -1 and scater_nodes_algo_index == m:
             # print(np.repeat(stamps,idle.shape[1]).shape,idle.flatten().shape)
             # p = pd.DataFrame()
             # p['node_idles'] = np.repeat(stamps,idle.shape[1])
-            # p['stamp'] = 
-            discrete = np.arange(0,stamps.shape[0],int(stamps.shape[0]/100))
-            idle = np.take(idle,discrete,axis=0)
-            stamps = np.take(stamps,discrete,axis=0)
-            fig.add_trace(go.Scattergl(x=np.repeat(stamps,idle.shape[1]) , y=idle.flatten(),
-                            mode='markers',
-                                                marker=dict(
-                            showscale=False,
-                            # colorscale options
-                            #'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
-                            #'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
-                            #'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
-                            colorscale='Jet',
-                            reversescale=False,
-                            size=5,
-                            opacity = 0.2,
-                            color=idle.flatten(),
-                            colorbar=dict(
-                                thickness=15,
-                                title="Instantaneous node idleness values",
-                                xanchor='left',
-                                titleside='right'
-                            ),
-                            line_width=0.5),legendgroup=100,showlegend=(True if idx==0 else False),name='{} Agents'.format(no_agents)),row=int(idx/col_size)+1,col=idx%col_size+1)  
-                            
-        
+            # p['stamp'] =
+            discrete = np.arange(0, stamps.shape[0], int(stamps.shape[0]/100))
+            idle = np.take(idle, discrete, axis=0)
+            stamps = np.take(stamps, discrete, axis=0)
+            fig.add_trace(go.Scattergl(x=np.repeat(stamps, idle.shape[1]), y=idle.flatten(),
+                                       mode='markers',
+                                       marker=dict(
+                showscale=False,
+                # colorscale options
+                # 'Greys' | 'YlGnBu' | 'Greens' | 'YlOrRd' | 'Bluered' | 'RdBu' |
+                # 'Reds' | 'Blues' | 'Picnic' | 'Rainbow' | 'Portland' | 'Jet' |
+                # 'Hot' | 'Blackbody' | 'Earth' | 'Electric' | 'Viridis' |
+                colorscale='Jet',
+                reversescale=False,
+                size=5,
+                opacity=0.2,
+                color=idle.flatten(),
+                colorbar=dict(
+                    thickness=15,
+                    title="Instantaneous node idleness values",
+                    xanchor='left',
+                    titleside='right'
+                ),
+                line_width=0.5), legendgroup=100, showlegend=(True if idx == 0 else False), name='{} Agents'.format(no_agents)), row=int(idx/col_size)+1, col=idx % col_size+1)
 
-        fig['layout']['xaxis'+str(idx+1)]['title']='Stamps'
-        fig['layout']['yaxis'+str(idx+1)]['title']='Instantaneous Graph ' + available_comparisons[comparison_parameter_index]
+        fig['layout']['xaxis'+str(idx+1)]['title'] = 'Stamps'
+        fig['layout']['yaxis'+str(idx+1)]['title'] = 'Instantaneous Graph ' + \
+            available_comparisons[comparison_parameter_index]
 
 # fig.update_layout(title='Instantaneous Graph '+ available_comparisons[comparison_parameter_index]+ ' Plot for ' + graph_name,title_x=0.5)
 # fig.update_layout(legend=dict(
@@ -100,18 +107,30 @@ for m,no_agents in enumerate(no_agents_list):
 #     x=0.5
 # ))
 fig.update_layout(legend=dict(title_font_family="Times New Roman",
-                              font=dict(size= 15)
-))
+                              font=dict(size=20, color='#ff0000')
+                              ))
+
+fig.update_layout(xaxis1_title=dict(font=dict(size=20)))
+fig.update_layout(xaxis2_title=dict(font=dict(size=20)))
+fig.update_layout(xaxis3_title=dict(font=dict(size=20)))
+
+fig.update_layout(yaxis1_title=dict(font=dict(size=20)))
+fig.update_layout(yaxis2_title=dict(font=dict(size=20)))
+fig.update_layout(yaxis3_title=dict(font=dict(size=20)))
+for i in fig['layout']['annotations']:
+    i['font'] = dict(size=25, color='#ff0000')
+iplot(fig)
+
 
 iplot(fig)
- 
+
 # file_name = ""
 # for idx,algo in enumerate(algo_list):
 #     if not idx:
 #         file_name = algo
 #     else:
 #         file_name = file_name + " | " + algo
-        
+
 # file_name = file_name + ".html"
 
 # if comparison_parameter_index == 0 :
